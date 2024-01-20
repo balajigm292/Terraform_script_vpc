3a8116477afc30af61be1285f9cb50ff6b084610
BALAJI G M <109514220+balajigm292@users.noreply.github.com>
2024-01-20 14:50:58 +0530

    Update Private_instance.tf

--- Private_subnet/Private_instance.tf
+++ Private_subnet/Private_instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "demo1-ec2" {
     ami = "ami-0c84181f02b974bc3"
     instance_type = "t2.micro"
-    key_name = "sigma"
+    key_name = "firstserver"
     count = "1"
   vpc_security_group_ids = ["${aws_security_group.rtp03-sg.id}"]
    subnet_id = "${aws_subnet.private_subnet.id}"
